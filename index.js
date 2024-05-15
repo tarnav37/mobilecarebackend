253199a3662cc94c5e2d6c2cd66d10f17782fa36
APPLE <apple@APPLEs-MacBook-Air-2.local>
2024-05-15 22:50:47 +0530

    some changes

--- index.js
+++ index.js
@@ -137,28 +137,27 @@ const express = require('express');
             //---------------Start of pdf template------------------------
             doc.text(`Customer Device Purchasing
 
-            All specifications on the contract are subject to the following terms and conditions. You agree to be bound to these terms when you sign below. These are the Terms and Conditions which you, the customer, agree to and are applicable to the purchase of your device by Mobile Care!
-            
-            
-            ➛ You the customer understand that Mobile Care is selling this device to you in "AS IS" conditions.
-            
-            ➛ Devices come certified pre-owned with original parts, unless specified by Mobile Care otherwise.
-            
-            ➛ All purchased devices come with a 30 Day warranty from Mobile Care.
-            
-            ➛ If you the customer damage the device, then Mobile Care is not responsible, but we can offer you a discount on repairs!
-            
-            ➛ All devices come carrier unlock!
-            
-            ➛ Device Unlock information is available upon request.
-            
-            ➛ Mobile Care is not responsible for carrier services that are not provided by us!
-            
-            ➛ Device purchasing by Debit/Credit Card will be subject to a ID verification and signature!
-            
-            ➛ Devices sold by cash will be checked for illegal tender!
-            
-            **Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any customer.**`,).moveDown();
+All specifications on the contract are subject to the following terms and conditions. You agree to be bound to these terms when you sign below. These are the Terms and Conditions which you, the customer, agree to and are applicable to the purchase of your device by Mobile Care!
+
+➛ You the customer understand that Mobile Care is selling this device to you in "AS IS" conditions.
+
+➛ Devices come certified pre-owned with original parts, unless specified by Mobile Care otherwise.
+
+➛ All purchased devices come with a 30 Day warranty from Mobile Care.
+
+➛ If you the customer damage the device, then Mobile Care is not responsible, but we can offer you a discount on repairs!
+
+➛ All devices come carrier unlock!
+
+➛ Device Unlock information is available upon request.
+
+➛ Mobile Care is not responsible for carrier services that are not provided by us!
+
+➛ Device purchasing by Debit/Credit Card will be subject to a ID verification and signature!
+
+➛ Devices sold by cash will be checked for illegal tender!
+
+**Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any customer.**`, { align: 'left', lineGap: 10 }).moveDown();
 
             //---------------Start of pdf template------------------------
             doc.text('Submitted Form Data', { align: 'center' }).moveDown();
@@ -196,7 +195,7 @@ const express = require('express');
             ➛ Mobile Care will validate your ID upon receiving the device.
             ➛ If you are under the age of 18, Mobile Care will not purchase any products from you. Parents and Guardians must be present.
             
-           NOTE: Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any such customer.`,).moveDown();
+           NOTE: Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any such customer.`,{ align: 'left', lineGap: 10 }).moveDown();
 
             //---------------Start of pdf template------------------------
             doc.text('Submitted Form Data', { align: 'center' }).moveDown();
@@ -283,7 +282,7 @@ const express = require('express');
     ➛ Mobile Care will dispose of any device, if you the customer leave the device for more than 45 days.
    
    NOTE: 
-    Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any such customer.`,).moveDown();
+    Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any such customer.`,{ align: 'left', lineGap: 10 }).moveDown();
 
             //---------------Start of pdf template------------------------
             doc.text('Submitted Form Data', { align: 'center' }).moveDown();
