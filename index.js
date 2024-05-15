4fe6096260a72f9346bc6aa6c5dc27578265118d
APPLE <apple@APPLEs-MacBook-Air-2.local>
2024-05-16 01:50:24 +0530

    new

--- index.js
+++ index.js
@@ -95,16 +95,13 @@ const express = require('express');
     });
     app.post('/sellingform', async (req, res) => {
         try {
-            const formData = req.body;
-            console.log(req.body)
-
-            const savedFormData = await SellingData.create(formData);
-
-            res.status(200).json({ message: 'Form data submitted successfully', formData: savedFormData });
+          const formData = req.body;
+          const savedFormData = await SellingData.create(formData);
+          res.status(200).json({ message: 'Form data submitted successfully', formData: savedFormData });
         } catch (error) {
-            res.status(500).json({ error: error.message });
+          res.status(500).json({ error: error.message });
         }
-    });
+      });
     app.post('/repairform', async (req, res) => {
         try {
             const formData = req.body;
@@ -181,7 +178,8 @@ All specifications on the contract are subject to the following terms and condit
         try {
             const formData = req.body;
             const doc = new PDFDocument();
-            
+            doc.text('Signature:', { align: 'center' }).moveDown();
+    doc.image(formData.signature, 100, 100);
             doc.fontSize(12);
 
             //---------------Start of pdf template------------------------
@@ -225,62 +223,30 @@ All specifications on the contract are subject to the following terms and condit
 
             //---------------Start of pdf template------------------------
             doc.text(`Repair Terms & Conditions
-            
-           
-    
-    All specifications on the contract are subject to the following terms and conditions. You agree to be bound to these terms when you sign below. These are the Terms and Conditions which you, the customer, agree to and are applicable to the repair of your device by Mobile Care:
-    
-    ➛ You the customer have agreed upon price as indicated prior to inspection and repair. If Mobile Care finds any underlying damage, Mobile Care is not responsible. New Price, Terms and Conditions will be agreed upon, if you the customer chooses to fix any such underlying damage.
-   
-    ➛ You the customer will be subjected to a minimum $25 diagnostic fee for any of the following reasons:
-    
-    ➛ Cleaning service of any major parts.
-   
-    ➛ Diagnostic services
+            All specifications on the contract are subject to the following terms and conditions. You agree to be bound to these terms when you sign below. These are the Terms and Conditions which you, the customer, agree to and are applicable to the repair of your device by Mobile Care:
+             ➛ You the customer have agreed upon price as indicated prior to inspection and repair. If Mobile Care finds any underlying damage, Mobile Care is not responsible. New Price, Terms and Conditions will be agreed upon, if you the customer chooses to fix any such underlying damage.
+             ➛ You the customer will be subjected to a minimum $25 diagnostic fee for any of the following reasons:
+             ➛ Cleaning service of any major parts.
+             ➛ Diagnostic services
+             ➛ If you choose to decline a repair after the service has been done, Mobile Care will charge you a $45 fee.
+             ➛ All screens come with a 30/Day Warranty, Mobile Care has the right to void warranty for any of the following reasons:
+             ➛ Customer phone is water damaged.
+             ➛ Customer has dropped phone, with indication of LCD and/or digitizer crack, blemishes, or scratches on screen.
+             ➛ Any underlying damage that has caused the screen to crack. (Ex: Bulging battery)
+             ➛ All parts come with a 30 day Warranty, Mobile Care has the right to void warranty for any of such reason: (Ex: Charging Port, Battery, Camera, etc).
+             ➛ Customer phone is water damaged.
+             ➛ Customer has damaged the part, or allowed another repair location that is not affiliated with Mobile Care to repair damage.
+             ➛ If Mobile Care, replaces your Proximity Sensor or Front Facing camera, Mobile Care is not responsible for Face Recognition (Face ID) being disabled. Replacing such part will disable such function.
+             ➛ All warranties repairs returning to any Mobile Care locations by customer, will be asked for proof of purchase. Mobile Care has the right to deny any service if proof of purchase is not shown.
+             ➛ Repairs done by Mobile Care will void manufacture warranty, but any repairs done by us will be backed by our Warranty Policy! (We are only affiliated with Akko Insurance) 
+             ➛ Any and all parts parts replaced on Apple Device are not genuine Apple Parts, but all parts are premium apple parts and will not affect the functionality of device, UNLESS part is related to functioning of Face ID.
+             ➛ All Android screens are genuine Android Displays, but all small parts are not genuine android parts, but all parts are premium android parts and will not affect the functionality of device.
+             ➛ Any such damage that we Mobile Care are responsible for, will be taken care of by Mobile Care, and will be back by our Warranty Policy.
+             ➛ Any and all devices will lose water resistance rating after damage and/ or repair. Mobile Care is not responsible for Water Damage! 
+             ➛ Mobile Care is not responsible for any device that is Disabled and/or Unavailable after repair.
+             ➛ Mobile Care is not responsible for any software or network related issues of Customer device.
+             ➛ Mobile Care will dispose of any device, if you the customer leave the device for more than 45 days.
 
-    ➛ If you choose to decline a repair after the service has been done, Mobile Care will charge you a $45 fee.
-   
-    ➛ All screens come with a 30/Day Warranty, Mobile Care has the right to void warranty for any of the following reasons:
-  
-    ➛ Customer phone is water damaged.
-   
-    ➛ Customer has dropped phone, with indication of LCD and/or digitizer crack, blemishes, or scratches on screen.
-  
-    ➛ Any underlying damage that has caused the screen to crack. (Ex: Bulging battery)
-
-    ➛ All parts come with a 30 day Warranty, Mobile Care has the right to void warranty for any of such reason: (Ex: Charging Port, Battery, Camera, etc)
-   
-    ➛ Customer phone is water damaged.
-  
-    ➛ Customer has damaged the part, or allowed another repair location that is not affiliated with Mobile Care to repair damage.
-   
-    ➛ If Mobile Care, replaces your Proximity Sensor or Front Facing camera, Mobile Care is not responsible for Face Recognition (Face ID) being disabled. Replacing such part will disable such function.
-
-    ➛ All warranties repairs returning to any Mobile Care locations by customer, will be asked for proof of purchase. Mobile Care has the right to deny any service if proof of purchase is not shown.
-  
-    ➛ Repairs done by Mobile Care will void manufacture warranty, but any repairs done by us will be backed by our Warranty Policy! (We are only affiliated with Akko
-    
-    Insurance)
-   
-    ➛ Any and all parts parts replaced on Apple Device are not genuine Apple Parts, but all parts are premium apple parts and will not affect the functionality of device,
-    
-    UNLESS part is related to functioning of Face ID.
-
-    ➛ All Android screens are genuine Android Displays, but all small parts are not genuine android parts, but all parts are premium android parts and will not affect the
-    
-    functionality of device.
-
-    
-    ➛ Any such damage that we Mobile Care are responsible for, will be taken care of by Mobile Care, and will be back by our Warranty Policy.
-
-    ➛ Any and all devices will lose water resistance rating after damage and/ or repair. Mobile Care is not responsible for Water Damage!
-    
-    ➛ Mobile Care is not responsible for any device that is Disabled and/or Unavailable after repair.
-
-    ➛ Mobile Care is not responsible for any software or network related issues of Customer device.
-    
-    ➛ Mobile Care will dispose of any device, if you the customer leave the device for more than 45 days.
-   
    NOTE: 
     Mobile Care abides by all local laws and city ordinances within the state and city limits of location. Mobile Care does not tolerate dishonesty and stolen devices, and forgery of any such customer.`,{ align: 'left', lineGap: 10 }).moveDown();
 
